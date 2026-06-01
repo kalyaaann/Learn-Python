970d5dfaef27260d3a568d8bef1983492317c775
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-01 10:02:06 +0545

    Operators and its types

---
+++ Day1/operators.py
@@ -0,0 +1,53 @@
+# #assignment operator
+# a=5 
+# b=2 
+# sum =a+b
+# print(sum)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+
+# print(a%b) #modulo
+# print(a**b) #power of number. a to the power b 
+
+# #relational operators
+ 
+# a=200
+# b=500
+# print(a == b)
+# print(a != b)
+# print( a>= b)
+# print(b <= 0)
+# print(b > a)
+
+
+#assignment operators
+ 
+# number=100
+# number = number + 20
+# print(number)
+
+# number += 30
+# print(number)
+
+# num=5
+# num **=5
+# print(num)
+
+
+# logical values 
+print(not True)
+print(not False)
+a=50
+b=40
+print (not a>b)
+print (not b>a)
+
+value1= True
+value2= False
+print("The AND operator:", value1 and value2)
+print("The OR operator:", value1 or value2)
+print("AND operator :", a>b and a==b)
+print("OR operator :", a>b or a==b)
+print("Condition checks of operator", value1==value2 or value1!=value2)
+
