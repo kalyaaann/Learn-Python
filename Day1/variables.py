e901edab2639c02500ff08d64de66f5f0c511261
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-01 10:02:31 +0545

    Variables in python

---
+++ Day1/variables.py
@@ -0,0 +1,22 @@
+# name="Kalyan"
+# age=19
+# sem=4
+# print("name")
+# print(name)
+# print(type(name))
+# print("My name is :",name)
+# print(age)
+# print(type(age))
+# print("sem",sem)
+
+
+# age_curr=age 
+# print(age_curr)
+
+class_today=False 
+learn_today=True
+plans_today =None
+
+print(type(class_today))
+print(type(plans_today))
+print(type(learn_today))
