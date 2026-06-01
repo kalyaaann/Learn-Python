fe224e9a4f59ca89172fdaf7bdc29b9f307db619
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-01 10:03:11 +0545

    Some simple programs in python

---
+++ Day1/simpleprograms.py
@@ -0,0 +1,47 @@
+# #print sum of numbers 
+# a=200
+# b=900
+# sum=a+b
+# print("The sum is :" ,sum)
+
+
+# #print difference of numbers 
+# a=2000
+# b=900
+# diff=a-b
+# print("The difference is :" ,diff)
+
+# #print simple interest 
+# p=18000
+# t=2
+# r=8
+# si=((p*t*r)/100)
+# print("The simple interst is :", si)
+# print("The amount is :",si+p)
+
+# #calculate area and volume
+# l=5
+# b=3
+# h=2
+# area =l*b
+# volume =l*b*h
+# print("The area is ",area)
+# print("The volume is :",volume)
+
+#typecasting and type conversion
+# a=200
+# b=50.3
+# sum=a+b
+# print(sum)
+
+# a=int('2')
+# b=4
+# sum=a+b
+# print(sum)
+
+#take input and calculate area of rectangle
+
+length =float(input("Enter length of rectangle : "))
+breadth = float(input("Enter breadth of the rectangle : "))
+area = length*breadth
+print("The area of the rectangle is ", area, "sq. units.")
