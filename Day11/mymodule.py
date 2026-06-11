9a04e7eee42539deb9b76cc46fbe861889f211c3
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-11 22:26:42 +0545

    Modules, packages and libraries in python

---
+++ Day11/mymodule.py
@@ -0,0 +1,14 @@
+#create a function 
+
+def say_hello(name):
+    return print(f"Hello,{name}!")
+
+def say_bye(name):
+    return print(f"ByeBye,{name}!")
+
+record={
+    'name':'Kalyan',
+    'age':'19',
+    'subject':'BSc CSIT',
+    'semester':4,
+    }
