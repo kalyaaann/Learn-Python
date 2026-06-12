1ea5d35a3cf243709205f6a30a6c52465838d14e
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-12 21:07:23 +0545

    Calculating Functions

---
+++ Day12/calculator.py
@@ -0,0 +1,22 @@
+#Simple Project : Calculator
+
+# Addition function
+def addition(number1,number2):
+     return number1 + number2 
+
+# Subtraction Function
+def subtraction(number1,number2):
+     return number1 - number2 
+
+# Multiplication function
+def multiplication(number1,number2):
+     return number1 * number2  
+
+# Division Function 
+def division(number1,number2):
+     return number1 / number2 
+
+# Average Function
+def average(number1,number2):
+     return (number1 + number2)/2  
+
