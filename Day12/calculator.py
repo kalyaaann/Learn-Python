f2c7f04bb2802174799342d314f317c89de9752b
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-12 21:08:30 +0545

    Calculator input

--- Day12/calculator.py
+++ Day12/calculator.py
@@ -20,3 +20,17 @@ def division(number1,number2):
 def average(number1,number2):
      return (number1 + number2)/2  
 
+
+#Taking the number input and operation input to perform from User
+
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+
+print("Please select a operation:\n " \
+      "1. Addition\n" \
+      "2. Substraction\n" \
+      "3. Multiplication\n" \
+      "4. Division\n" \
+      "5. Average\n") 
+
+select = int(input("Enter the number of operation you want to perform : ")) 
