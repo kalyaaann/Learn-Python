7ecad926910304fabd376705632c4c85a29dd0ce
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-21 18:51:56 +0545

    Image drawing in opencv

--- Day21/draw.py
+++ Day21/draw.py
@@ -1,24 +1,22 @@
-    import cv2
+import cv2
 
-    image= cv2.imread("pythonimg.png")
+image= cv2.imread("pythonimg.png")
 
-    if image is None:
-        print("Error! Image not found")
-    else:
-        print("Image Loaded Successfully")
-        pt1=(50,100)
-        pt2=(300,100)
-        color=(0,255,0)
-        thickness=2
-        line_img= cv2.line(image,pt1, pt2,color,thickness)
+if image is None:
+    print("Error! Image not found")
+else:
+    print("Image Loaded Successfully")
+    pt1=(50,100)
+    pt2=(300,100)
+    color=(0,255,0)
+    thickness=2
+    line_img= cv2.line(image,pt1, pt2,color,thickness)
 
-        cv2.imshow("Line draw",line_img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    cv2.imshow("Line draw",line_img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-
-    #rectangle draw
-
+#rectangle draw
 
     ptr1=(50,100)
     ptr2=(300,200)
@@ -31,19 +29,19 @@
     cv2.destroyAllWindows()
 
 
-    #circle draw
+#circle draw
 
 
-    circle_draw= cv2.circle(image,(150,150),50,(255,0,0),2)
+circle_draw= cv2.circle(image,(150,150),50,(255,0,0),2)
 
-    cv2.imshow("Circle draw",circle_draw)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+cv2.imshow("Circle draw",circle_draw)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-    #Adding texts to image
+#Adding texts to image
 
-    add_text= cv2.putText(image,"Python Logo",(100,200),cv2.FONT_HERSHEY_DUPLEX,1.5,(0,0,255),3)
+add_text= cv2.putText(image,"Python Logo",(100,200),cv2.FONT_HERSHEY_DUPLEX,1.5,(0,0,255),3)
 
-    cv2.imshow("Adding Text in image",add_text)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+cv2.imshow("Adding Text in image",add_text)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
