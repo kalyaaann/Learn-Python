18dc1d034578373f159f24477dc127fffacdb4d4
kalyaaan24 <kalyandhamala24@gmail.com>
2026-06-22 21:39:36 +0545

    Save Captured video using OpenCV in python

---
+++ Day22/savevideo.py
@@ -0,0 +1,26 @@
+import cv2
+
+camera=cv2.VideoCapture(0)
+
+frame_width= int (camera.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height= int (camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+codec= cv2.VideoWriter_fourcc(*'XVID')
+recorded= cv2.VideoWriter("myvideo.avi",codec,20,(frame_width,frame_height))
+
+while True:
+    success, image= camera.read()
+
+    if not success:
+        break
+    
+    recorded.write(image)
+    cv2.imshow("Recording Live",image)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+camera.release()
+recorded.release()
+cv2.destroyAllWindows()
+    
